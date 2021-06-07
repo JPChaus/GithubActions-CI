217487ccdf17cef3c7c8f32f288b703bde8798e7
JPChaus <48576328+JPChaus@users.noreply.github.com>
2021-06-07 01:19:38 -0400

    fix linting

--- test.py
+++ test.py
@@ -8,7 +8,6 @@ class Testcase(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calc.addition(1, 2), 3)
 
-
     def test_subtract(self):
         self.assertEqual(calc.subtract(5, 3), 2)
 
