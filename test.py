d909a081cb9b12935990d31bd9ee874625e6276f
JPChaus <48576328+JPChaus@users.noreply.github.com>
2021-06-07 01:37:02 -0400

    added multiply test

--- test.py
+++ test.py
@@ -11,6 +11,9 @@ class Testcase(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(calc.subtract(5, 3), 2)
 
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(4, 3), 12)
+
 
 if __name__ == '__main__':
     unittest.main()
